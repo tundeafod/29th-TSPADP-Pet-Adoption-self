e857550963da25d1ceec3e9a6dbed10c0c410579
tundeafod <tunde.afod@gmail.com>
2024-05-02 17:51:34 +0100

    code update

--- module/sonarqube/main.tf
+++ module/sonarqube/main.tf
@@ -6,14 +6,21 @@ resource "aws_instance" "sonarqube_server" {
   subnet_id                   = var.subnet_id
   associate_public_ip_address = true
   user_data                   = local.sonarqube_user_data
-
   tags = {
     Name = var.name
   }
 }
 
 
-#CLI command that would create s3 bucket and Dynamo db
-# aws s3api create-bucket --bucket your-terraform-state-bucket-name --region your-region --create-bucket-configuration LocationConstraint=your-region
 
-# aws dynamodb create-table --table-name your-terraform-state-lock-table --attribute-definitions AttributeName=LockID,AttributeType=S --key-schema AttributeName=LockID,KeyType=HASH --billing-mode PAY_PER_REQUEST
+#Delete S3 Bucket & Dynamo DB table (Delete Dynamo DB table first)
+# aws s3 rb s3://tfstate-tspadp --force
+# aws dynamodb delete-table --table-name tspadp-backend --region eu-west-2
+
+#CLI command that would create s3 bucket and Dynamo db
+# aws s3api create-bucket --bucket tfstate-tspadp --region eu-west-2 --create-bucket-configuration LocationConstraint=your-region
+
+# aws dynamodb create-table --table-name your table name --attribute-definitions AttributeName=LockID,AttributeType=S \
+#     --key-schema AttributeName=LockID,KeyType=HASH \
+#     --billing-mode PAY_PER_REQUEST \
+#     --region your region
--- module/sonarqube/sonarqube-script.tf
+++ module/sonarqube/sonarqube-script.tf
@@ -6,7 +6,7 @@ locals {
 sudo apt update -y
 
 # Install necessary packages
-sudo apt install -y openjdk-11-jdk postgresql unzip net-tools nginx
+sudo apt install -y openjdk-11-jdk postgresql unzip
 
 # Configure OS level values
 sudo bash -c 'echo "
@@ -19,6 +19,16 @@ sudo bash -c 'echo "
 sonarqube   -   nofile   65536
 sonarqube   -   nproc    4096" >> /etc/security/limits.conf'
 
+# Install PostgreSQL
+echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/postgresql.asc
+sudo apt update -y
+sudo apt install -y postgresql-16 postgresql-contrib-16
+
+# Enable and start PostgreSQL
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+
 # Change default password of postgres user
 sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'Admin123@'"
 
@@ -30,20 +40,25 @@ sudo -u postgres psql -c "ALTER USER sonar WITH ENCRYPTED PASSWORD 'Admin123@'"
 sudo -u postgres psql -c "CREATE DATABASE sonarqube OWNER sonar"
 sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE sonarqube to sonar"
 
-# Download and extract SonarQube
-sudo mkdir /opt/sonarqube/
-sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.4.1.35646.zip -P /opt/sonarqube/
-sudo unzip /opt/sonarqube/sonarqube-9.4.1.35646.zip -d /opt/sonarqube/
-sudo mv /opt/sonarqube/sonarqube-9.4.1.35646 /opt/sonarqube/
+# Restart PostgreSQL for changes to take effect
+sudo systemctl restart postgresql
+
+# Install SonarQube
+sudo mkdir /sonarqube/
+cd /sonarqube/
+sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.6.1.59531.zip
+sudo unzip sonarqube-9.6.1.59531.zip -d /opt/
+sudo mv /opt/sonarqube-9.6.1.59531/ /opt/sonarqube
+
 
 # Add group user sonarqube
-sudo groupadd sonarqube
+sudo groupadd sonar
 
 # Create a user and add the user into the group with directory permission to the /opt/ directory
-sudo useradd -m -c "SonarQube - User" -d /opt/sonarqube/ -g sonarqube sonarqube
+sudo useradd -c "SonarQube - User" -d /opt/sonarqube/ -g sonar sonar
 
-# Change ownership of the directory to sonarqube
-sudo chown sonarqube:sonarqube /opt/sonarqube/ -R
+# Change ownership of the directory to sonar
+sudo chown sonar:sonar /opt/sonarqube/ -R
 
 # Configure SonarQube
 sudo bash -c 'echo "
@@ -63,8 +78,8 @@ Type=forking
 ExecStart=/opt/sonarqube/bin/linux-x86-64/sonar.sh start
 ExecStop=/opt/sonarqube/bin/linux-x86-64/sonar.sh stop
 ExecReload=/opt/sonarqube/bin/linux-x86-64/sonar.sh restart
-User=sonarqube
-Group=sonarqube
+User=sonar
+Group=sonar
 Restart=always
 LimitNOFILE=65536
 LimitNPROC=4096
@@ -78,12 +93,18 @@ sudo systemctl daemon-reload
 sudo systemctl enable sonarqube.service
 sudo systemctl start sonarqube.service
 
-# Configure Nginx to access SonarQube server from outside
+# Install net-tools incase we want to debug later
+sudo apt install net-tools -y
+
+# Install nginx
+sudo apt install nginx -y
+
+# Configure nginx to access server from outside
 sudo tee /etc/nginx/sites-enabled/sonarqube.conf > /dev/null <<EOT
 server {
   listen 80;
-  access_log  /var/log/nginx/sonarqube.access.log;
-  error_log   /var/log/nginx/sonarqube.error.log;
+  access_log  /var/log/nginx/sonar.access.log;
+  error_log   /var/log/nginx/sonar.error.log;
   proxy_buffers 16 64k;
   proxy_buffer_size 128k;
   location / {
@@ -101,11 +122,13 @@ EOT
 # Remove the default configuration file
 sudo rm /etc/nginx/sites-enabled/default
 
-# Reload Nginx service
-sudo systemctl reload nginx.service
+# Enable and restart nginx service
+sudo systemctl enable nginx.service
+sudo systemctl stop nginx.service
+sudo systemctl start nginx.service
 
 # Install New Relic
-# curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY="${var.nr-key}" NEW_RELIC_ACCOUNT_ID="${var.nr-acc-id}" NEW_RELIC_REGION="${var.nr-region}" /usr/local/bin/newrelic install -y
+#curl -Ls https://download.newrelic.com/install/newrelic-cli/scripts/install.sh | bash && sudo NEW_RELIC_API_KEY="${var.nr-key}" NEW_RELIC_ACCOUNT_ID="${var.nr-acc-id}" NEW_RELIC_REGION="${var.nr-region}" /usr/local/bin/newrelic install -y
 
 # Change Hostname(IP) to something readable
 sudo hostnamectl set-hostname Sonarqube

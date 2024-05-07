dfbf9364302ef7a57395070d90f52928b3472714
tundeafod <tunde.afod@gmail.com>
2024-05-07 17:46:41 +0100

    code commit

--- root/output.tf
+++ root/output.tf
@@ -5,7 +5,12 @@ output "Sonarqube-ip" {
 output "bastion-ip" {
   value = module.bastion.bastion_ip
 }
-
 output "nexus-ip" {
   value = module.nexus.nexus_ip
+}
+output "jenkins-ip" {
+  value = module.jenkins.jenkins_ip
+}
+output "ansible-ip" {
+  value = module.ansible.ansible_ip
 }
